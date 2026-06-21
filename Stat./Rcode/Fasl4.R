a4932a70d5d67b7829a0b646684ef5a4c1fb8631
pouniq <abdollahpora@gmail.com>
2026-06-21 18:47:03 +0330

    writing functions is Done the next section is Statistical Inference

--- Stat./Rcode/Fasl4.R
+++ Stat./Rcode/Fasl4.R
@@ -88,6 +88,31 @@ f <- function(x,y=6){
 f(2)
 
 
+
+# ساخت تابع برای محاسبه محیط و مساحت
+rect <- function(a,b){
+  p = 2 * (a+b) # محیط مستطیل
+  s = a * b # مساحت مستطیل
+  c(Perimeter=p,Area=s)
+}
+rect(2,3)
+
+
+# مساحت رویه و حجم مکعب را محاسبه کنیم.
+cu <- function(x,y,z){
+  v <- x * y * z # محاسبه حجم
+  s <- (x*y+x*z+y*z)*2 # مساحت سطح
+  c(volume = v, Surface_Area = s)
+  
+}
+cu(2,4,2)
+
+
+# اگر به آرگومان ها عدد بدهیم به آنها آرگومان اختیاری گفته میشود
+# ولی اگر عددی ندهیم باید در زمان فراخوانی برای آن عدد تعریف کنیم
+
+
+
 ##### if statements
 
 x<- 10
@@ -127,20 +152,129 @@ f <- function(x,y, R){
 }
 f(x,y,R)
 
-
+# تابع نشانگر 
+# Indicator function
 gammalik <- function(theta, x){
   alpha <- theta[1]
   beta <- theta[2]
   
   z <- (1/gamma(alpha)*beta^alpha)*x^(alpha - 1)*exp(-x/beta)
-  y<- ifelse(x >= 0, z, 0)
+  y <- ifelse(x >= 0, z, 0)
   prod(y)
 }
+x <- c(2,3,5,6)
+gammalik(c(2,5),x)
+log(gammalik(c(2,5),x))
+
+# if we minimum the -f, we actually maximize the f
+
+obj <- function(theta){
+  -log(gammalik(theta,x))
+}
+# distr
+optim(c(2,1),obj)
+
+# باید یک مقدار اولیه به آلفا و بتا بدیم تا همگرا شود.
+# par: نشان دهنده مقدار برآورد شده آلفا و بتا است
+# value: نشان دهنده مقدار تابع وقتی که آلفا و بتای می نیمم شده را در آن قرار می دهیم
+# counts: چندبار الگوریتم تکرار شده تا به نتیجه برسه
+# convergence: نشان دهنده این هست که آیا همگرا شده یا خیر که اگر صفر باشد همگرا نشده
+# و اگر ۱ باشد همگرا شده است
+
+ 
+
+# Dist. Detection -----
+# ما یک بردار مشاهدات داریم میخوایم ببینیم که آیا از یک توزیع آمدند یا خیر
+# استفاده از تابع چندک چندک
+qqdist <- function(x, dist, ...){
+  pois <- 
+}
+
+# function complete that.
 
 
-gammalik(c(3,1),x)
 
-optim()
+
+# For Loop و حلقه ----
+## For: برای تمام چیزهایی که داریم روش کار انجام میشه -- تعداد تکرارها ثابت باشه
+## While: تا موقعی که اون شرطه درسته ادامه بده-- اگر تعداد تکرارها مشخص نباشه ولی نیازه که به یک شرطی برسیم
+## repeat: مثل همون وایل هست ولی شرطو توی بدنه میزاریم نه همون اول
+
+
+
+# Fibonacci ----
+
+fib <- rep(1, 100)
+for (i in 3:1000){
+  fib[i] <- fib[i-1] + fib[i-2]
+}
+fib
+
+# While Or Repeat:
+# Taylor
+
+s <- 0
+n <- 0
+
+while(abs(s-sqrt(3)/2) >= 1e-12){
+  s <- s + ((-1)^n / factorial(2*n +1) * (pi/3)^(2*n+1))
+  n <- n+1
+}
+s
+n
+
+## we can write that with repeat too !!!!!
+# we just reverse the condition in repeat
+
+s <- 0
+n <- 0
+
+repeat{
+  s <- s + ((-1)^n / factorial(2*n +1) * (pi/3)^(2*n+1))
+  n <- n+1
+  
+  if(abs(s-sqrt(3)/2) < 1e-12)
+    break
+}
+s
+n
+
+
+
+
+# گشتاور ----
+# گشتاورهای مرتبه ۱ تا آر رو می خوایم بنویسیم
+mom <- function(x, r){
+  m <- rep(0,1)
+  for(k in 1:r){
+    m[k] <- mean(x^k)
+  }
+  m
+}
+mom(x,5)
+
+
+# برای گشتاور مرکزی
+mom <- function(x, r){
+  m <- rep(0,1)
+  for(k in 1:r){
+    m[k] <- mean((x-mean(x))^k)
+  }
+  m
+}
+mom(x,5)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # تمرین -------
